9b01d8ceeac99deeeef7087ad55b2d402c57cfcd
bellamard kiala <belamard@outlook.fr>
2025-05-31 09:59:58 +0100

    feat(fr):menu turnoff

--- src/main/java/com/b2la/dbroffice/controller/DashboardController.java
+++ src/main/java/com/b2la/dbroffice/controller/DashboardController.java
@@ -1,7 +1,36 @@
 package com.b2la.dbroffice.controller;
 
-public class DashboardController{
+import com.b2la.dbroffice.preference.Storage;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class DashboardController implements Initializable {
+
+    private Label username, countAdmin, countOffice, countAgents, countClients, countUsers,
+            countSend, countWithdrawal, countDeposit, countFactory,
+            amountSendUSD, amountWithdrawalUSD, amountDepositUSD,
+            amountSendCDF, amountWithdrawalCDF, amountDepositCDF;
+    private Button btnTaux;
+    private TextField searchTaux;
+    private TableView tableTaux;
 
 
 
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+    }
+
+    @FXML
+    protected void onClose() {
+        Storage.removeLogin();
+        System.exit(0);
+    }
 }
